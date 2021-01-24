fc003a9e5bbc551a29a200184d09dd93ed8cfc54
ihitrec <hitrec555@gmail.com>
2021-01-24 22:57:51 +0000

    Start numpad creation. Add a function that remembers last cell. Enable last cell input by pressing numpad numbers.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,10 @@
 // Enable input on all table cells
 $("td").attr("contenteditable", "true");
 
-// Add a key up function on each cell
-$("td").attr("onkeyup", "myFunc()");
+// Add a key up function on each cell, if entered key is a number, keep it. Otherwise delete it.
+$("td").attr("onkeyup", "isNum()");
 
-// If entered key is a number, keep it. Otherwise delete it.
-function myFunc() {
+function isNum() {
     let whichCell = this.event.path[0];
     if (Number.isNaN(parseInt(this.event.key)) == false && this.event.key !== "0") {
         let numKey = this.event.key;
@@ -14,4 +13,25 @@ function myFunc() {
     } else if (Number.isNaN(parseInt(this.event.key)) == true || this.event.key === "0") {
         whichCell.innerText = "";
     }
+}
+
+// Add a click function on each cell. Push the last selected cell into an array.
+
+$("td").attr("onclick", "selectedCell()")
+
+let lastPressed = [];
+function selectedCell() {
+    lastPressed = [];
+    lastPressed.push(this.event.path[0])
+    return lastPressed;
+
+}
+
+// Enter the pressed number on numpad into the last selected cell.
+
+$("p").attr("onclick", "numPad()")
+
+function numPad() {
+    console.log(lastPressed);
+    lastPressed[0].innerText = this.event.path[0].innerText;
 }
