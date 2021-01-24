19ba294ce56d9774a8f886bbde1543acdbd80128
ihitrec <hitrec555@gmail.com>
2021-01-24 22:09:40 +0000

    Disable input of zeros in grid. Adjust grid style.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,10 @@ $("td").attr("onkeyup", "myFunc()");
 // If entered key is a number, keep it. Otherwise delete it.
 function myFunc() {
     let whichCell = this.event.path[0];
-    if (Number.isNaN(parseInt(this.event.key)) == false) {
+    if (Number.isNaN(parseInt(this.event.key)) == false && this.event.key !== "0") {
         let numKey = this.event.key;
         whichCell.innerText = numKey;
-    } else if (Number.isNaN(parseInt(this.event.key)) == true) {
+    } else if (Number.isNaN(parseInt(this.event.key)) == true || this.event.key === "0") {
         whichCell.innerText = "";
     }
 }
