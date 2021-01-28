84b923af29081de0ccafbda9687bb4914c5e734b
ihitrec <hitrec555@gmail.com>
2021-01-28 22:45:02 +0000

    Adjust mobile keyboard function, code cleanup.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,46 @@
+$(document).ready(function () {
 
-// Enable input on all table cells
-$("td").attr("contenteditable", "true");
+    // Enable input on all table cells
+    $("td").attr("contenteditable", "true");
 
-// Add a key up function on each cell, if entered key is a number, keep it. Otherwise delete it.
-$("td").attr("onkeyup", "isNum()");
+    // Add a key up function on each cell, if entered key is a number, keep it. Otherwise delete it.
+    $("td").keyup(isNum);
 
-function isNum() {
-    let whichCell = this.event.path[0];
-    if (Number.isNaN(parseInt(this.event.key)) == false && this.event.key !== "0") {
-        let numKey = this.event.key;
-        whichCell.innerText = numKey;
-    } else if (Number.isNaN(parseInt(this.event.key)) == true || this.event.key === "0") {
-        whichCell.innerText = "";
+    function isNum() {
+        let whichCell = this;
+        if (Number.isNaN(parseInt(event.key)) == false && event.key !== "0") {
+            let numKey = event.key;
+            whichCell.innerText = numKey;
+        } else if (Number.isNaN(parseInt(event.key)) == true || event.key === "0") {
+            whichCell.innerText = "";
+        }
     }
-}
 
-//  Disables phone keyboard from popping up
-function disableMobile() {
-    setTimeout(function () {
+    //  Disable phone keyboard from popping up
+    function disableMobile() {
         $("td").attr("contenteditable", "false");
-        $("td").attr("contenteditable", "true");
-    });
-}
-
-// Add a click function on each cell. Push the last selected cell into an array. 
-// Call disableMobile on selected screens.
-$("td").attr("onclick", "selectedCell()");
-
-let lastPressed = [];
-function selectedCell() {
-    lastPressed = [];
-    lastPressed.push(this.event.path[0]);
-    if (window.innerWidth < 1000) {
-        disableMobile();
+        setTimeout(function () {
+            $("td").attr("contenteditable", "true");
+        }, 100);
     }
-}
 
-// Enter the pressed number on numpad into the last selected cell
-$(".num").attr("onclick", "numPad()");
+    // Add a click function on each cell. Push the last selected cell into an array. 
+    // Call disableMobile on selected screens.
+    $("td").click(selectedCell);
 
-function numPad() {
-    console.log(lastPressed);
-    lastPressed[0].innerText = this.event.path[0].innerText;
-}
+    let lastPressed = [];
+    function selectedCell() {
+        lastPressed = [];
+        lastPressed.push(this);
+        if (window.innerWidth < 1000) {
+            disableMobile();
+        }
+    }
+
+    // Enter the pressed number on numpad into the last selected cell
+    $(".num").click(numPad);
+    function numPad() {
+        lastPressed[0].innerText = this.innerText;
+    }
+});
 
