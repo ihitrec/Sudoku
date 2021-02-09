02d9f275ca1714ab3a7139d978e4abdca329d14e
ihitrec <hitrec555@gmail.com>
2021-02-09 04:16:02 +0000

    feat(box animation): light up box green when last cell in it is filled

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,6 +159,7 @@ $(document).ready(function () {
         // Get cells in the same box
         let whichBox = [];
         let sameBox = [];
+        let sameBoxText = [];
         let boxes = [[0, 1, 2, 9, 10, 11, 18, 19, 20], [3, 4, 5, 12, 13, 14, 21, 22, 23], [6, 7, 8, 15, 16, 17, 24, 25, 26], [27, 28, 29, 36, 37, 38, 45, 46, 47], [30, 31, 32, 39, 40, 41, 48, 49, 50], [33, 34, 35, 42, 43, 44, 51, 52, 53], [54, 55, 56, 63, 64, 65, 72, 73, 74], [57, 58, 59, 66, 67, 68, 75, 76, 77], [60, 61, 62, 69, 70, 71, 78, 79, 80]];
         for (b = 0; b < 9; b++) {
             if ((boxes[b]).includes(cellArray.indexOf(whichCell)) === true) {
@@ -170,6 +171,7 @@ $(document).ready(function () {
         }
         for (b1 = 0; b1 < 8; b1++) {
             sameBox.push(cellArray[whichBox[b1]]);
+            sameBoxText.push((cellArray[whichBox[b1]]).innerText);
         }
 
         // Executes if inputed cell is wrong in the loop below.
@@ -233,6 +235,11 @@ $(document).ready(function () {
                 if (sameColText.includes("") === false) {
                     for (h = 0; h < 8; h++) {
                         time(h, sameCol);
+                    }
+                }
+                if (sameBoxText.includes("") === false) {
+                    for (j = 0; j < 8; j++) {
+                        time(j, sameBox);
                     }
                 }
                 $(whichCell).addClass("correct");
