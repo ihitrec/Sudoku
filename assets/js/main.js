6e602f5f309626d41f122040198f22e47b357299
ihitrec <hitrec555@gmail.com>
2021-02-09 00:40:00 +0000

    feat(box-check): add same box and selected cell value check

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,6 +156,22 @@ $(document).ready(function () {
             }
         }
 
+        // Get cells in the same box
+        let whichBox = [];
+        let sameBox = [];
+        let boxes = [[0, 1, 2, 9, 10, 11, 18, 19, 20], [3, 4, 5, 12, 13, 14, 21, 22, 23], [6, 7, 8, 15, 16, 17, 24, 25, 26], [27, 28, 29, 36, 37, 38, 45, 46, 47], [30, 31, 32, 39, 40, 41, 48, 49, 50], [33, 34, 35, 42, 43, 44, 51, 52, 53], [54, 55, 56, 63, 64, 65, 72, 73, 74], [57, 58, 59, 66, 67, 68, 75, 76, 77], [60, 61, 62, 69, 70, 71, 78, 79, 80]];
+        for (b = 0; b < 9; b++) {
+            if ((boxes[b]).includes(cellArray.indexOf(whichCell)) === true) {
+                whichBox = whichBox.concat(boxes[b]);
+                let removeSelf = whichBox.indexOf(cellArray.indexOf(whichCell));
+                whichBox.splice(removeSelf, 1);
+                break;
+            }
+        }
+        for (b1 = 0; b1 < 8; b1++) {
+            sameBox.push(cellArray[whichBox[b1]]);
+        }
+
         // Executes if inputed cell is wrong in the loop below.
         function itsWrong(p1, i) {
             $(p1[i]).attr("contenteditable", "false");
@@ -188,7 +204,10 @@ $(document).ready(function () {
         // Otherwise mark it as correct.
         let actualValue = intermediate2Solved[cellArray.indexOf(whichCell)].toString();
         for (i = 0; i < 8; i++) {
-            if ($(sameCol[i]).text() === whichNum) {
+            if ($(sameBox[i]).text() === whichNum) {
+                itsWrong(sameBox, i);
+                break;
+            } else if ($(sameCol[i]).text() === whichNum) {
                 itsWrong(sameCol, i);
                 break;
             } else if ($(sameRow[i]).text() === whichNum) {
